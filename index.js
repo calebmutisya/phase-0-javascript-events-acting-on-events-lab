5391e951f71009825aa7975b7046e26484fd22c1
calebmutisya <mutisyacalebmusau@gmail.com>
2023-09-17 14:54:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft(){
+    const currentPosition =parseInt(dodger.style.left) || 0;
+    const newPosition = Math.max(currentPosition - 10, 0);
+    dodger.style.left = newPosition + 'px';
+}
+
+function moveDodgerRight(){
+    const currentPosition =parseInt(dodger.style.left) || 0;
+    const newPosition = Math.min(currentPosition + 10, 360);
+    dodger.style.left = newPosition + 'px';
+}
